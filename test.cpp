65a4d915203cf2ec83de14d6f8ee24150564b55d
Mladen <{ID}+{username}@users.noreply.github.com>
2021-05-27 00:01:40 +0200

    Updated test.cpp to test saving data to file

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@
 #include "Stan.h"
 #include "Stanar.h"
 #include "Load_data.h"
+#include "Save_data.h"
 
 // #include <fstream>
 
@@ -22,15 +23,54 @@ int main()
     //This call will cause Error -1073741819 when try to access pointer to 'stan' (which is not yet initialized)
     // PrintStan(stan);
 
-    
 
     SpisakStanara::ReadZgradaFromFile(v_zgrada);
 
+    // Look if stan_br 9 is already added if previously stan_br 10 was added
+    bool is_stan9 = false;
+    for (int i = 0; i < v_zgrada.size(); i++)
+    {
+        if(v_zgrada[i].getBrojStan() == 9)
+        {
+            is_stan9 = true;
+        }
+        if ((v_zgrada[i].getBrojStan() == 10) && (!is_stan9))
+        {
+            is_stan9 = true;
+        }
+    }
+
+    if(is_stan9)
+    {
+        SpisakStanara::Stan stan9;
+        SpisakStanara::Stanar stanar9;
+        // Add stan_br 9
+        stan9.setBrojStan(9);
+        stan9.setPovrsina(20.0);
+        stanar9.setStan(stan9);
+
+        stanar9.setIme("Ime9");
+        stanar9.setPrezime("Prezime9");
+        stanar9.setTelBroj("009/11-11-111");
+        stanar9.setEmail("ime9@mail.com");
+
+        stan9.setStanari().push_back(stanar9);
+
+        stanar9.setIme("Ime9_2");
+        stanar9.setPrezime("Prezime9_2");
+        stanar9.setTelBroj("009/22-22-222");
+        stanar9.setEmail("ime9_2@mail.com");
+
+        stan9.setStanari().push_back(stanar9);
+
+        v_zgrada.push_back(stan9);
+        // Added stan_br 9
+    }
+
+    // Add stan_br 10
     stan.setBrojStan(10);
     stan.setPovrsina(25.0);
     stanar.setStan(stan);
-
-    // PrintStan(stan);
 
     stanar.setIme("Ime1");
     stanar.setPrezime("Prezime1");
@@ -47,10 +87,12 @@ int main()
     stan.setStanari().push_back(stanar);
 
     v_zgrada.push_back(stan);
-    // PrintStan(stan);
+    // Added stan_br 10
 
     PrintZgrada(v_zgrada);
     std::cout << std::endl;
+
+    SpisakStanara::WriteZgradaToFile(v_zgrada);
 
     std::cout << "END_OF_PROGRAM\n";
     // std::ifstream file;
